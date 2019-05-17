c7eb43d84d4ac28fa1530e0e36e925de09741841
davidoyeku <oyeku@dxc.com>
2019-05-17 15:59:39 +0000

    change compartment build to pull from master, removed branch option so pull from master by default

--- pipelines/compartment-build-prod.groovy
+++ pipelines/compartment-build-prod.groovy
@@ -12,7 +12,7 @@ pipeline{
     stages{
         stage('Check Out SCM'){
             steps{
-                git url: 'e4s@vs-ssh.visualstudio.com:v3/e4s/E4S-PublicCloud/SystemTeam', credentialsId: 'daivdprivatekeygit', branch: 'davidasg-refactor'
+                git url: 'e4s@vs-ssh.visualstudio.com:v3/e4s/E4S-PublicCloud/SystemTeam', credentialsId: 'daivdprivatekeygit'
             }
          
         }
