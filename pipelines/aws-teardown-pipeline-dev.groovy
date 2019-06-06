b410a4a9533851fdd20dfed02fb80ccde08f686a
davidoyeku <oyeku@dxc.com>
2019-06-06 09:23:11 +0000

    modified teardown

--- pipelines/aws-teardown-pipeline-dev.groovy
+++ pipelines/aws-teardown-pipeline-dev.groovy
@@ -14,7 +14,7 @@ pipeline{
       string defaultValue: 'Z19T3FXREVEDR9', description: 'enter vpc long name id. Use the VPC-ID to find longname ID in route53', name: 'vpc_longname_id', trim: true
       string defaultValue: 'Z19T3DERRVEDR9', description: 'in-addr.arpa VPC-ID to find longname ID in route53', name: 'in_addr_arpa', trim: true
       string defaultValue: 'vgw-0f73c280059aa00d2', description: 'gateway ID', name: 'gateway_id', trim: true
-      choice choices: ['true', 'false'], description: 'tear down DR', name: 'dr',
+      choice choices: ['true', 'false'], description: 'tear down DR', name: 'dr'
       string defaultValue: 'vpc-0af9bf39247934d58', description: 'Enter the DR VPC ID', name: 'dr_vpc_id', trim: true
       string defaultValue: 'DR-Dev-Example', description: 'Enter the VPC Name here', name: 'dr_vpc_name', trim: true
       string defaultValue: 'us-east-2', description: 'Enter the region here', name: 'dr_region', trim: true
